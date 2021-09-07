509e9da62b4e67a2b80d81cfda7f16edbeb8ff58
Syifa Ainnur <arwahitem01@gmail.com>
2021-09-07 22:20:16 +0700

    file tugas

---
+++ Assets/SideWall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWall : MonoBehaviour
+{
+    public PlayerControl player;
+    // Skrip GameManager untuk mengakses skor maksimal
+    [SerializeField]
+    private GameManager gameManager; 
+        // Akan dipanggil ketika objek lain ber-collider (bola) bersentuhan dengan dinding.
+    void OnTriggerEnter2D(Collider2D anotherCollider)
+    {
+        // Jika objek tersebut bernama "Ball":
+        if (anotherCollider.name == "Ball")
+        {
+            // Tambahkan skor ke pemain
+            player.IncrementScore();
+ 
+            // Jika skor pemain belum mencapai skor maksimal...
+            if (player.Score < gameManager.maxScore)
+            {
+                // ...restart game setelah bola mengenai dinding.
+                anotherCollider.gameObject.SendMessage("RestartGame", 2.0f, SendMessageOptions.RequireReceiver);
+            }
+        }
+    }
+}
